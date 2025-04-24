e7465a0698a425ab15cac30ea01c1222247beba4
ArmaanSinghKlair <armaansinghkl@gmail.com>
2025-04-23 21:29:44 -0600

    Reduced image sizes. Also constrained java jvm sizes.

--- realtime-app-docker-config/realtime-webapp-combined-image/multi_war_startup.sh
+++ realtime-app-docker-config/realtime-webapp-combined-image/multi_war_startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Start service registry in the background
-java -jar realtime-service-registry.war &
+java -Xmx64m -XX:NewSize=16m -XX:MaxNewSize=16m -XX:MetaspaceSize=48m -XX:MaxMetaspaceSize=64m -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -jar realtime-service-registry.war &
 
 # Start microservice instance 1 (port 8000) in the background
-java -Dserver.port=8000 -Dlog.port=8000 -jar realtime-microservice.war #&
+java -Xmx72m -XX:NewSize=20m -XX:MaxNewSize=20m -XX:MetaspaceSize=67m -XX:MaxMetaspaceSize=100m -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -Dserver.port=8000 -Dlog.port=8000 -jar realtime-microservice.war #&
 
 # Start microservice instance 2 (port 8001) in the foreground (keeps container alive)
 #java -Dserver.port=8001 -Dlog.port=8001 -jar realtime-microservice.war
